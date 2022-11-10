9ca1f6ca579d5ff8524d92d562915708f2ac19fc
jjaquezrd <102117463+jjaquezrd@users.noreply.github.com>
2022-11-09 21:54:41 -0400

    Subiendo archivo ejercicio 1. Hola mundo

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hola mundo") #1
+nombre="joel" #2
+print("Hola", nombre) #2a
+print("Hola " + nombre) #2b
+numero_favorito = 7  
+print("Hello {{",7,"}}") #3a
+print("Hello {{" + str(7) +"}}") #3b
+fav_food1= "platanos maduros"
+fav_food2= "Empanadas"
+print("Amo comer {} y {}".format(fav_food1,fav_food2)) #4a
+print(f"Amo comer {fav_food1} y {fav_food2}.") #4b
+print ("Hola",nombre.upper())
+print ("Mi nombre en minuscula es", nombre.lower())
